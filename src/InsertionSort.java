ef4420a4b6477a8c3486986881de8acb7c9dac8b
System-in-a-System <textconstruction@protonmail.com>
2020-07-07 12:15:56 +0300

    Adding method to carry out insertion sort on the array of strings

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,5 +1,5 @@
+import java.util.Comparator;
 
-	
 // Public class to hold insertion sort algorithms for the array of int and the array of strings
 
 // Insertion sort sorts the array in multiple phases, 
@@ -53,4 +53,56 @@ public class InsertionSort {
 		
 		return safeArrayCopy;
 	}
+	
+	
+	
+	
+	// Method to carry out insertion sort on the array of strings
+	public static String[] insertionSort(String[] in, Comparator<String> c) {
+		
+		// Make a safe copy of the array (original input array shall not be mutated)
+		String[] safeArrayCopy = new String[in.length];
+				
+		for (int i = 0; i < safeArrayCopy.length; i++)
+			safeArrayCopy[i] = in[i];
+			
+		
+		// If the array's length is 1 or 0, return the array at this point
+		if (safeArrayCopy.length <= 1)
+			return safeArrayCopy;
+		
+		
+				
+		// Set the default value of next pass "switcher" 
+		boolean needNextPass =  true;
+				
+				
+		// Outer loop to handle insertion progression along the array
+		for (int k = 1; k < safeArrayCopy.length && needNextPass; k++) {
+					
+			// Set assumption that the array is already sorted, so the next pass will not be needed
+			// The assumption will be changed to "true" in case a swapping case will take place within the current loop 
+			needNextPass = false;
+					
+					
+			// Inner loop to handle comparison of neighboring elements in the array
+			for (int i = 0; i < safeArrayCopy.length - k; i++) {
+						
+				// If neighboring elements are in descending order
+				if (c.compare(safeArrayCopy[i], safeArrayCopy[i + 1]) < 0) {
+							
+							// Swap the neighboring elements so the order is ascending
+							String temp = safeArrayCopy[i];
+							safeArrayCopy[i] = safeArrayCopy[i + 1];
+							safeArrayCopy[i + 1] = temp;
+							
+							// Since the swapping took place, the next pass is needed
+							needNextPass = true;
+				}
+			}
+		}
+				
+		return safeArrayCopy;
+	}
+	
 }
